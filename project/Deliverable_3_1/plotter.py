ca68ddf5b642dd424ec9a7394589c9315b4a2842
Killian Baillifard <killian.baillifard@gmail.com>
2025-12-31 15:33:47 +0100

    Enhanced plots

--- project/Deliverable_3_1/plotter.py
+++ project/Deliverable_3_1/plotter.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
 import numpy as np
 from LinearMPC.MPCVelControl import MPCVelControl
 import os
@@ -20,7 +21,7 @@ def save_fig(name: str) -> None:
     PLOT_DIR = 'plots/'
     plt.tight_layout()
     os.makedirs(PLOT_DIR, exist_ok=True)
-    plt.savefig(PLOT_DIR + file_name(name))
+    plt.savefig(PLOT_DIR + file_name(name), dpi=200)
     plt.show()
 
 def plot_x_y_terminal_set(name: str, mpc: MPCVelControl) -> None:
@@ -58,55 +59,72 @@ def plot_x_y_terminal_set(name: str, mpc: MPCVelControl) -> None:
 
     save_fig(name)
 
+
+def enforce_min_yrange(ax: Axes, min_range: float = 2):
+    y0, y1 = ax.get_ylim()
+    rng = y1 - y0
+    if rng < min_range:
+        mid = 0.5 * (y0 + y1)
+        half = 0.5 * min_range
+        ax.set_ylim(mid - half, mid + half)
+
 def plot_traj(name: str, x: np.ndarray, u: np.ndarray, t: np.ndarray) -> None:
 
     start_fig(name)
     X_COLOR = 'xkcd:red'
     Y_COLOR = 'xkcd:green'
     Z_COLOR = 'xkcd:blue'
+    PRIMARY_COLOR = 'orange'
+    SECONDARY_COLOR = 'teal'
 
-    plt.subplot(221)
-    plt.plot(t[:-1], x[9, :-1], color=X_COLOR, linestyle=(0, (1, 3)), label=r'$p_x$')
-    plt.plot(t[:-1], x[10, :-1], color=Y_COLOR, linestyle=(2, (1, 3)), label=r'$p_y$')
-    plt.plot(t[:-1], x[11, :-1], color=Z_COLOR, label=r'$p_z$')
-    plt.ylabel(r'$p_x, \quad p_y, \quad p_z \quad (m)$')
-    plt.grid()
-    plt.legend(loc='upper right')
+    ax = plt.subplot(221)
+    ax.plot(t[:-1], x[9, :-1], color=X_COLOR, linestyle=(0, (1, 3)), label=r'$p_x$')
+    ax.plot(t[:-1], x[10, :-1], color=Y_COLOR, linestyle=(2, (1, 3)), label=r'$p_y$')
+    ax.plot(t[:-1], x[11, :-1], color=Z_COLOR, label=r'$p_z$')
+    ax.set_ylabel(r'$p_x, \quad p_y, \quad p_z \quad (m)$')
+    ax.grid()
+    ax.legend(loc='upper right')
+    enforce_min_yrange(ax)
 
     ax = plt.subplot(222)
     twin = ax.twinx()
     ax.plot(t[:-1], np.rad2deg(x[3, :-1]), color=X_COLOR, label=r'$\alpha$')
     ax.plot(t[:-1], np.rad2deg(x[4, :-1]), color=Y_COLOR, label=r'$\beta$')
     ax.plot(t[:-1], np.rad2deg(x[5, :-1]), color=Z_COLOR, label=r'$\gamma$')
-    twin.plot(t[:-1], np.rad2deg(x[0, :-1]), color='xkcd:magenta', label=r'$\omega_\alpha$')
-    twin.plot(t[:-1], np.rad2deg(x[1, :-1]), color='xkcd:cyan', label=r'$\omega_\beta$')
-    twin.plot(t[:-1], np.rad2deg(x[2, :-1]), color='xkcd:purple', label=r'$\omega_\gamma$')
+    twin.plot(t[:-1], np.rad2deg(x[0, :-1]), color=X_COLOR, linestyle=(0, (3, 3)), label=r'$\omega_\alpha$')
+    twin.plot(t[:-1], np.rad2deg(x[1, :-1]), color=Y_COLOR, linestyle=(0, (3, 3)), label=r'$\omega_\beta$')
+    twin.plot(t[:-1], np.rad2deg(x[2, :-1]), color=Z_COLOR, linestyle=(0, (3, 3)), label=r'$\omega_\gamma$')
     ax.set_ylabel(r'$\alpha, \quad \beta, \quad \gamma \quad (^\circ)$')
     twin.set_ylabel(r'$\omega_x, \quad \omega_y, \quad \omega_z \quad (^\circ/s)$')
     ax.legend(loc='upper right')
     twin.grid()
     twin.legend(loc='lower right')
+    enforce_min_yrange(ax)
+    enforce_min_yrange(twin)
 
-    plt.subplot(223)
-    plt.plot(t[:-1], x[6, :-1], color=X_COLOR, linestyle=(0, (1, 3)), label=r'$v_x$')
-    plt.plot(t[:-1], x[7, :-1], color=Y_COLOR, linestyle=(2, (1, 3)), label=r'$v_y$')
-    plt.plot(t[:-1], x[8, :-1], color=Z_COLOR, label=r'$v_z$')
-    plt.xlabel(r'$t \quad (s)$')
-    plt.ylabel(r'$v_x, \quad v_y, \quad v_z \quad (m/s)$')
-    plt.grid()
-    plt.legend(loc='upper right')
+    ax = plt.subplot(223)
+    ax.plot(t[:-1], x[6, :-1], color=X_COLOR, linestyle=(0, (1, 3)), label=r'$v_x$')
+    ax.plot(t[:-1], x[7, :-1], color=Y_COLOR, linestyle=(2, (1, 3)), label=r'$v_y$')
+    ax.plot(t[:-1], x[8, :-1], color=Z_COLOR, label=r'$v_z$')
+    ax.set_xlabel(r'$t \quad (s)$')
+    ax.set_ylabel(r'$v_x, \quad v_y, \quad v_z \quad (m/s)$')
+    ax.grid()
+    ax.legend(loc='upper right')
+    enforce_min_yrange(ax)
 
     ax = plt.subplot(224)
     twin = ax.twinx()
-    ax.plot(t[:-1], np.rad2deg(u[0, :]), color=X_COLOR, label=r'$\delta_1$')
-    ax.plot(t[:-1], np.rad2deg(u[1, :]), color=Y_COLOR, label=r'$\delta_2$')
-    twin.plot(t[:-1], u[2, :], color='xkcd:teal', label=r'$P_\text{avg}$')
-    twin.plot(t[:-1], u[3, :], color='xkcd:blue', label=r'$P_\text{diff}$')
+    ax.plot(t[:-1], np.rad2deg(u[0, :]), color=PRIMARY_COLOR, linestyle=(0, (1, 3)), label=r'$\delta_1$')
+    ax.plot(t[:-1], np.rad2deg(u[1, :]), color=SECONDARY_COLOR, linestyle=(2, (1, 3)), label=r'$\delta_2$')
+    twin.plot(t[:-1], u[2, :], color=PRIMARY_COLOR, label=r'$P_\text{avg}$')
+    twin.plot(t[:-1], u[3, :], color=SECONDARY_COLOR, label=r'$P_\text{diff}$')
     ax.set_xlabel(r'$t \quad (s)$')
     ax.set_ylabel(r'$\delta_1, \quad \delta_2 \quad (^\circ)$')
     twin.set_ylabel(r'$P_\text{avg}, \quad P_\text{diff} \quad (\%)$')
     ax.legend(loc='upper right')
     twin.grid()
     twin.legend(loc='lower right')
+    enforce_min_yrange(ax)
+    enforce_min_yrange(twin)
 
     save_fig(name)
